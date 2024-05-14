cce440c8c0ec09e8a332f44bb9f3eb9d28129ba1
huangrd <huangrd@ziroom.com>
2024-05-14 17:35:10 +0800

    bug: Error when importing multiple components from third-party packages in dev mode

--- src/main.js
+++ src/main.js
@@ -2,8 +2,10 @@ import './style.css';
 import { createApp } from 'vue';
 import App from './App.vue';
 import { Button } from 'vant';
+// import { Button, Calendar } from 'vant'; 
 import 'vant/lib/index.css';
 
 
 
 createApp(App).use(Button).mount('#root');
+// createApp(App).use(Button).use(Calendar).mount('#root');
